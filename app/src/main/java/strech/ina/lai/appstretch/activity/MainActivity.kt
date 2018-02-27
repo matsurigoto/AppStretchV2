3344ab43458caa5f2f9bab787ef88f981463edf2
matsurigoto <dog0416@gmail.com>
2018-02-27 14:47:38 +0800

    Add MenuActivity.kt update MainActivity.kt

--- app/src/main/java/strech/ina/lai/appstretch/activity/MainActivity.kt
+++ app/src/main/java/strech/ina/lai/appstretch/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package strech.ina.lai.appstretch.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import strech.ina.lai.appstretch.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener{
+            _ -> startActivity( Intent( this, MenuActivity::class.java))
+        }
     }
 }
