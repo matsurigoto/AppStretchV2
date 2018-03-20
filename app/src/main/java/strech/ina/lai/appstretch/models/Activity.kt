b026856c9be147ce77f3e6fd6d016aded04adcda
matsurigoto <dog0416@gmail.com>
2018-03-20 15:12:22 +0800

    add Activity.kt

---
+++ app/src/main/java/strech/ina/lai/appstretch/models/Activity.kt
@@ -0,0 +1,6 @@
+package strech.ina.lai.appstretch.models
+
+/**
+ * Created by DuranHsieh on 2018/3/20.
+ */
+class Activity (val id: Int, val title: String, val imageUrl: String, val description: String)
