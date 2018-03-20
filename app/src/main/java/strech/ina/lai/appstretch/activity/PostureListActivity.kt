61f4eefd98d3d4ea452b0ef29e8d80596efc8025
matsurigoto <dog0416@gmail.com>
2018-03-20 14:46:03 +0800

    polishing code

--- app/src/main/java/strech/ina/lai/appstretch/activity/PostureListActivity.kt
+++ app/src/main/java/strech/ina/lai/appstretch/activity/PostureListActivity.kt
@@ -2,8 +2,11 @@ package strech.ina.lai.appstretch.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_posture_list.*
 import strech.ina.lai.appstretch.adapters.PostureListAdapter
 import strech.ina.lai.appstretch.R
+import strech.ina.lai.appstretch.models.Posture
 
 class PostureListActivity : AppCompatActivity() {
 
@@ -13,8 +16,11 @@ class PostureListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posture_list)
 
-        //this.postureListAdapter = PostureListAdapter(this, WeatherService.threeDayForecast)
-        //recycler_view_posture_list.adapter = this.postureListAdapter
-        //recycler_view_posture_list.layoutManager = LinearLayoutManager(this)
+        // fake data
+        val postureList = ArrayList<Posture>()
+        
+        this.postureListAdapter = PostureListAdapter(this, postureList)
+        recycler_view_posture_list.adapter = this.postureListAdapter
+        recycler_view_posture_list.layoutManager = LinearLayoutManager(this)
     }
 }
