ed44120180eea1472bde23f630f2dcd8ae2c6a7b
matsurigoto <dog0416@gmail.com>
2018-03-23 15:55:13 +0800

    Update PostureDetail xml

---
+++ app/src/main/java/strech/ina/lai/appstretch/activity/PostureDetailActivity.kt
@@ -0,0 +1,13 @@
+package strech.ina.lai.appstretch.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import strech.ina.lai.appstretch.R
+
+class PostureDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posture_detail)
+    }
+}
