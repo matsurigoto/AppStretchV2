405229a614fc785287b4dfcaf4c6c3c6941d5b44
Duran Hsieh <dog0416@gmail.com>
2018-06-17 14:48:31 +0800

    two column for posture

--- app/src/main/java/strech/ina/lai/appstretch/activity/PostureListActivity.kt
+++ app/src/main/java/strech/ina/lai/appstretch/activity/PostureListActivity.kt
@@ -2,11 +2,12 @@ package strech.ina.lai.appstretch.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_posture_list.*
 import strech.ina.lai.appstretch.adapters.PostureListAdapter
 import strech.ina.lai.appstretch.R
 import strech.ina.lai.appstretch.models.Posture
+import android.support.v7.widget.GridLayoutManager
+
 
 class PostureListActivity : AppCompatActivity() {
 
@@ -26,7 +27,8 @@ class PostureListActivity : AppCompatActivity() {
         postureList.add(Posture(id = 0,title = "Posture6", imageUrl="https://distudio.blob.core.windows.net/study4tw/1517584022.78193.jpg",description = "Posture6"))
         
         this.postureListAdapter = PostureListAdapter(this, postureList)
+
         recycler_view_posture_list.adapter = this.postureListAdapter
-        recycler_view_posture_list.layoutManager = LinearLayoutManager(this)
+        recycler_view_posture_list.layoutManager = GridLayoutManager(this, 2)
     }
 }
