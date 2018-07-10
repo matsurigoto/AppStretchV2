b5ebb072301bb873e38c11393ea89ddb9f1a3206
matsurigoto <dog0416@gmail.com>
2018-07-10 21:49:23 +0800

    debug

--- app/src/main/java/strech/ina/lai/appstretch/activity/TipListActivity.kt
+++ app/src/main/java/strech/ina/lai/appstretch/activity/TipListActivity.kt
@@ -2,12 +2,12 @@ package strech.ina.lai.appstretch.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_posture_list.*
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_tip_list.*
 import strech.ina.lai.appstretch.R
 import strech.ina.lai.appstretch.adapters.TipListAdapter
 import strech.ina.lai.appstretch.models.Tip
+import strech.ina.lai.appstretch.utils.getString
 
 class TipListActivity : AppCompatActivity() {
 
@@ -18,10 +18,16 @@ class TipListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_tip_list)
 
         val tipList = ArrayList<Tip>()
-
+        // fake data
+        for (i in 1..3)
+        {
+            val title = getString("posture" + i.toString().padStart(2, '0') + "_title")
+            val imageUrl = getString("posture" + i.toString().padStart(2, '0') + "_url")
+            val description = getString("posture" + i.toString().padStart(2, '0') + "_description")
+            tipList.add(Tip(id = i,title = title, imageUrl=imageUrl, description = description))
+        }
         this.tipListAdapter = TipListAdapter(this, tipList)
-        recycler_view_posture_list.adapter = this.tipListAdapter
-        recycler_view_posture_list.layoutManager = GridLayoutManager(this, 2) as RecyclerView.LayoutManager?
-
+        recycler_view_tip_list.adapter = this.tipListAdapter
+        recycler_view_tip_list.layoutManager = LinearLayoutManager(this)
     }
 }
