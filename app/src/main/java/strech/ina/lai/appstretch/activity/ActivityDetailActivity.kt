45bbc6656b37fd881d85cb81bf9f801f79450cb4
matsurigoto <dog0416@gmail.com>
2018-07-10 23:40:19 +0800

    update view

--- app/src/main/java/strech/ina/lai/appstretch/activity/ActivityDetailActivity.kt
+++ app/src/main/java/strech/ina/lai/appstretch/activity/ActivityDetailActivity.kt
@@ -1,7 +1,8 @@
 package strech.ina.lai.appstretch.activity
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import kotlinx.android.synthetic.main.activity_detail.*
 import strech.ina.lai.appstretch.R
@@ -14,5 +15,12 @@ class ActivityDetailActivity : AppCompatActivity() {
 
         val adRequest = AdRequest.Builder().build()
         adView!!.loadAd(adRequest)
+
+        val path = "android.resource://" + packageName + "/" + R.raw.activity001
+        videoView?.setVideoURI(Uri.parse(path))
+        videoView.start();
+        //videoView?.setVideoPath("")
+
+        videoView
     }
 }
